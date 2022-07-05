38b3f42093d5b8d187620546f6a37c9b11ead418
Matt Clarke <matt.clarke85@gmail.com>
2022-07-05 16:43:19 +1000

    reversion

--- lib/merchant_warrior/payment_request.rb
+++ lib/merchant_warrior/payment_request.rb
@@ -2,14 +2,5 @@ require "active_record"
 
 class MerchantWarrior::PaymentRequest < ActiveRecord::Base
   self.table_name = "merchant_warrior_payment_requests"
-  validates :mw_transaction_id, presence: true
-  validates :repayment_id, presence: true
   validates :status, presence: true
-
-  def self.format_status(provider_response)
-    case provider_response
-    when "Transaction pending" then "pending"
-    end
-  end
-
 end
